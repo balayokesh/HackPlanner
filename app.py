08a2c6d09ca232d3161f8d48dca2a5d647400394
balayokesh <balayokeshmani@gmail.com>
2023-07-05 19:57:14 +0530

    implemented routes

--- app.py
+++ app.py
@@ -1,7 +1,29 @@
 from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
+tasks = []
 
 @app.route('/')
 def index():
-    return "Hello, world!"
+    return render_template('index.html', tasks=tasks)
+
+@app.route('/add', methods=['POST'])
+def add_task():
+    task = request.form['task']
+    tasks.append(task)
+    return redirect('/')
+
+@app.route('/edit/<int:task_id>', methods=['GET, POST'])
+def edit_task(task_id):
+    if (request.method == 'POST'):
+        new_task = request.form['task']
+        tasks[task_id - 1] = new_task
+        return redirect('/')
+    
+    task = tasks[task_id - 1]
+    return render_template('edit.html', task=task, task_id=task_id)
+
+@app.route('/delete/<int:task_id>')
+def delete_task(task_id):
+    del tasks[task_id - 1]
+    return redirect('/')
